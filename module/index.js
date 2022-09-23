9093f1edb0c1bfe3152c99eef8c82122f90e6c62
李国烨 <guohua.li@patpat.com>
2022-09-23 10:22:46 +0800

    发表文章

--- module/index.js
+++ module/index.js
@@ -12,3 +12,15 @@ let UserSchema = new mongoose.Schema({
 let User = mongoose.model('User', UserSchema)
 //把用户模型挂载导出对象上
 exports.User = User
+
+let ObjectId = mongoose.Schema.Types.ObjectId
+//发表文章
+let articleSchema = new mongoose.Schema({
+    title: String,
+    content: { type: String },
+    createAt: { type: Date, default: Date.now },
+    //User 是一个外链,引用的是另一个集合的主键
+    user: { type: ObjectId, ref: 'User' },//引用的是用户表的_id
+})
+let Article = mongoose.model('Article', articleSchema)
+exports.Article = Article
--- routes/article.js
+++ routes/article.js
@@ -1,11 +1,24 @@
 let express = require('express')
 let router = express.Router()
 let { checkLogin } = require('../auth')
+let { Article } = require('../module/index')
 router.get('/add', checkLogin, function (req, res) {
     //路由是相对路径, 相对于模板的根目录
     res.render('article/add.html', {
         title: '发表文章'
     })
 })
-
+router.post('/add', checkLogin, function (req, res) {
+    let article = req.body
+    article.user = req.session.user._id
+    Article.create(article, function (err, doc) {
+        if (err) {
+            req.flash('error', err)
+            res.redirect('back')
+        } else {
+            req.flash('success', '文章发表成功')
+            res.redirect('/')
+        }
+    })
+})
 module.exports = router
