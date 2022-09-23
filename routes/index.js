3a2828ed81e7a38e0ad6801e15b3b87c61084604
李国烨 <guohua.li@patpat.com>
2022-09-23 15:45:18 +0800

    搜索文章功能

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,23 @@ let { Article } = require('../module')
 let router = express.Router()
 //当客户端通过GET请求访问/路径的时候,会交由对应的函数来处理
 router.get('/', function (req, res) {
+    let { keyword } = req.query
+    let query = {}
+    if (keyword) {
+        // query.title = new RegExp(keyword);// /b/
+        query['$or'] = [
+            { title: new RegExp(keyword) },
+            { content: new RegExp(keyword) },
+        ]
+    }
     //populate 可以把一个字段从字符串转成对象
-    Article.find().populate('user').exec(function (err, articles) {
-        console.log('aarjlkdsjf', articles)
+    Article.find(query).populate('user').exec(function (err, articles) {
         if (err) {
             req.flash('error', '操作数据库失败')
         } else {
             if (articles) {
                 //路由是相对路径, 相对于模板的根目录
-                res.render('index.html', { title: '首页', articles })
+                res.render('index.html', { title: '首页', articles, keyword })
             } else {
                 req.flash('error', '未查询到相关文章')
             }
